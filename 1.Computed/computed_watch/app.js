4c9a2839394f9d2df2355892feb70f9d224b6c8a
raja0612 <rajashekhar.ramayampeta@gmail.com>
2018-08-18 16:31:29 -0400

    added watch property explanation

--- 1.Computed/computed_watch/app.js
+++ 1.Computed/computed_watch/app.js
@@ -4,7 +4,10 @@ new Vue ({
         computed : 'Computed Propeties are cached based upon thier dependcies(data properties) '
         +'and computed proprties are re evaluated only when thier depencies are changed',
         watch: 'Vue Does provide a more generic way to observe and react to data changes on Vue Instance',
-        message : 'Hello Vue Js World'
+        message : 'Hello Vue Js World',
+        a: 0,
+        b: 0,
+        sum: 0
     },
     computed : {
         reversedMessage: function () {
@@ -17,5 +20,18 @@ new Vue ({
             console.log('messageReverse method')
             return this.message.split('').reverse().join(' ');
         }
+    },
+    watch: {
+        a:function(value){
+            console.log('a is changed ', value);
+            this.a = value;
+            this.sum = this.a + this.b;
+        },
+        b:function(value){
+            this.b = value;
+            console.log('b is changed', value);
+            this.sum = this.a + this.b;
+        }
+
     }
 })
