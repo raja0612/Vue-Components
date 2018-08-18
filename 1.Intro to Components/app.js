eb01196aed530fab16df9815b952b34795534391
raja0612 <rajashekhar.ramayampeta@gmail.com>
2018-08-18 16:54:15 -0400

    learning vue components

---
+++ 1.Intro to Components/app.js
@@ -0,0 +1,15 @@
+Vue.component('button-counter', {
+    data: function () {
+      return {
+        count: 0
+      }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+  })
+
+new Vue ({
+    el:'#app',
+    data: {
+
+    }
+})
