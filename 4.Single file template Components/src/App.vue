6ea166297d133690ad20d0d5a342371490a231f2
raja0612 <rajashekhar.ramayampeta@gmail.com>
2018-08-25 14:03:36 -0400

    find the solution s for office task

--- 4.Single file template Components/src/App.vue
+++ 4.Single file template Components/src/App.vue
@@ -2,10 +2,32 @@
    <div class="container">
        <p></p>
        <p>This is root componennt</p>
-       <button @click="showServer(1)">Show Server1</button>
+        <button @click="showServer(1)">Show Server1</button>
         <button @click="showServer(2)">Show Server2</button>
        <div v-for="i in 2">
            <app-server v-if="i === serverNumber" :server="i"></app-server>
+       </div>
+
+       <div>
+           Steps to follow for Office work:
+
+           1. On CoordinatorConfernces component
+
+           Iterate all coordinator ranks child components 
+           based upon coordinator no of conferences 
+           and intially hide them.
+
+           <div v-for="i in noOfConferenceLength">
+               <coordinator-ranks  v-if="i === serverNumber" :componentNumber="i">
+               </coordinator-ranks>
+           </div>
+           <div v-on:click="firstFunction(); secondFunction();"></div>
+           2. When the user clicks on particular data table confernce row
+           pass index number bacause it;s unique
+           <coordinator-ranks :componentNumber="index"></coordinator-ranks>
+           in coordinator-ranks component
+           declare props property add :componentNumber
+
        </div>
        
    </div>
